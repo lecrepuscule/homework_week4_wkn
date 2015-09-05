79aab829891d392c08e5e3b1b268cbb8dfc1b62c
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-05 16:24:03 +0100

    created several erb page files and completed basic contents in layout

--- memetube/lib/app.rb
+++ memetube/lib/app.rb
@@ -3,7 +3,22 @@ require "sinatra/reloader" if developoment?
 require "pry"
 require "pg"
 
+before do
+  @db = PG.connect(dbname: "memetube_app", host: "localhost")
+end
+
+after do
+  @db.close
+end
+
 #index
+get "/" do
+  @header = my videos
+  sql = "select title from videos"
+  @videos = @db.exec(sql)  
+  erb :videos
+end
+
 
 #new
 
